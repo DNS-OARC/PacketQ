04912c2613dbbeb0475802782fdd0b20e00d736f
Henrik Levkowetz <henrik@netnod.se>
2013-09-16 15:02:21 +0200

    Updated regression-test.sh to make the ordering of test query results more consistent, to avoid false positives.

--- src/regression-test.sh
+++ src/regression-test.sh
@@ -36,7 +36,7 @@ for SQL in \
 	'select rcode as Rcode, msg_size as ReplyLen, count(1) as count from dns where qr==1 group by Rcode,ReplyLen order by count desc ;' \
 	"select 'ALL' as All, rd as RD, count(1) as count from dns where qr==0 group by All,RD order by count desc ;" \
 	"select if(protocol==6,'tcp',if(protocol==17,'udp',protocol)) as Transport, qtype as Qtype, count(1) as count from dns where qr==0 group by Transport,Qtype order by Transport,Qtype,count desc ;" \
-	"select s, dst_addr as Dst_addr, qtype as questiontype, lower(src_addr) as lower_src, if(1 and s < 1 or s <= 1 or s > 1 or s >= 1, 't', 'f'), trim(trim('foofoo' || rsplit(src_addr, 1) || 'foofoo', 'foo'), 'bar'), count(*), len(src_addr), sum(msg_size + -1 - 2 % 4 << 3 >> 2 | 3 & ~4) + 1, min(msg_size), max(msg_size), truncate(1.1) as integer, 1.1 as float, sum(src_port + 1.0 - 2.0 / 1.5 * -2.5) + 1.0, max(src_port + 1.0), min(src_port + 1.0), avg(src_port), stdev(src_port), name('rcode', 0) from dns where src_addr like '%' and (qr or not qr) group by src_addr, s having s >= 0 order by s, lower_src, integer, float" \
+	"select s, dst_addr as Dst_addr, qtype as questiontype, lower(src_addr) as lower_src, if(1 and s < 1 or s <= 1 or s > 1 or s >= 1, 't', 'f'), trim(trim('foofoo' || rsplit(src_addr, 1) || 'foofoo', 'foo'), 'bar'), count(*), len(src_addr), sum(msg_size + -1 - 2 % 4 << 3 >> 2 | 3 & ~4) + 1, min(msg_size), max(msg_size), truncate(1.1) as integer, 1.1 as float, sum(src_port + 1.0 - 2.0 / 1.5 * -2.5) + 1.0, max(src_port + 1.0), min(src_port + 1.0), avg(src_port), stdev(src_port), name('rcode', 0) from dns where src_addr like '%' and (qr or not qr) group by src_addr, s having s >= 0 order by s, dst_addr, lower_src, integer, float" \
         ;
 do
     test=$test+1
@@ -44,8 +44,9 @@ do
     echo ""
     if [ ${#SQL} -gt 200 ]; then ellipsis="..."; else ellipsis=""; fi
     echo "Test $test: '${SQL:0:200}$ellipsis'"
-    t_new=$(/usr/bin/time -f "%e" ./packetq -s "$SQL" $1 2>&1 > $DIR/$new.test$test.result)
+    t_new=$(/usr/bin/time -f "%e" ./packetq --tlds /usr/share/packetq/tlds -s "$SQL" $1 2>&1 > $DIR/$new.test$test.result)
     e_new=$?
+    echo "  Comparing $new against available binaries:"
     for prev in $(ls ../../packetq*/src/packetq); do
 	old=$($prev --version | cut -d " " -f 2)
 	ver=$(printf "%-20s" $old)
